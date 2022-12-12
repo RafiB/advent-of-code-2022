371fafceddf2f50cef0ba44cb7f82cfb815ee95c
Raf Blecher <raf@commonroom.io>
2022-12-11 21:29:36 -0800

    day 12 star 2

--- day-12.py
+++ day-12.py
@@ -49,20 +49,22 @@ abcccccccaaaaaaaaccccccccccccccccaaaaaaaaaaaaacccccccccccccccccccccccccccccccccc
 
 def solve(puzzle_input):
     M = [[c for c in row] for row in puzzle_input.split("\n")]
+    q = []
 
     for y, row in enumerate(M):
         for x, c in enumerate(row):
-            if c == "S":
-                start = (x, y, 0)
+            if c == "S" or c == "a":
+                q.append((x, y, 0))
 
+    answer = float('inf')
     seen = set()
-    q = [start]
-    while True:
+    while len(q) > 0:
         x, y, s = q.pop(0)
         if (x, y) in seen:
             continue
         if M[y][x] == "E":
-            return s
+            answer = min(answer, s+1)
+            continue
 
         seen.add((x, y))
 
@@ -83,19 +85,21 @@ def solve(puzzle_input):
                 cVal = "z"
 
             if M[y + dy][x + dx] == "E" and M[y][x] in ('y', 'z'):
-                return s + 1
+                answer = min(answer, s+1)
+                continue
 
             if ord(cVal) - ord(tVal) < -1:
                 continue
 
-            q.append((x + dx, y + dy, s + 1))
+            q.append((x + dx, y + dy, s + 1 if cVal != "a" else 1))
 
         # M[y][x] = '.'
         # for row in M:
         #     print("".join(row))
+    return answer
 
 
 
 if __name__ == "__main__":
-    assert solve(TEST_INPUT) == 31, solve(TEST_INPUT)
+    assert solve(TEST_INPUT) == 29, solve(TEST_INPUT)
     print(solve(PUZZLE_INPUT))
